22db72925b742b2cc43720047a9f53a13f35f873
Eric Curtin <ecurtin@redhat.com>
2022-08-27 11:24:24 +0100

    Remove assert introduce perror
    Introduce perror and remove assert

--- ostree-compliance-mode.c
+++ ostree-compliance-mode.c
@@ -1,9 +1,21 @@
-#include <assert.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#ifndef PREFIX
+#define PREFIX "/usr"
+#endif
+
+#define PERR(...)                \
+  do {                           \
+    char* str;                   \
+    asprintf(&str, __VA_ARGS__); \
+    perror(str);                 \
+    free(str);                   \
+  } while (0)
 
 static void print_help() {
   printf("%s\n%s\n", "Usage:", "  ostree-compliance-mode enable");
@@ -14,7 +26,10 @@ int main(const int argc, const char* argv[]) {
   act.sa_handler = SIG_IGN;
   for (int i = 1; i < 65; ++i) {
     if (i != SIGKILL && i != SIGSTOP && i != SIGCHLD && i != 32 && i != 33) {
-      assert(sigaction(i, &act, NULL) == 0);
+      if (sigaction(i, &act, NULL) < 0) {
+        PERR("sigaction(%d, %p, NULL)", i, &act);
+        return errno;
+      }
     }
   }
 
@@ -35,8 +50,7 @@ int main(const int argc, const char* argv[]) {
 
   static const char* file = PREFIX "/libexec/ostree-compliance-mode-helper";
   if (execl(file, file, "enable", (char*)NULL) < 0) {
-    printf("execl(\"%s\", \"%s\", \"enable\", (char*)NULL) = %d\n", file, file,
-           errno);
+    PERR("execl(\"%s\", \"%s\", \"enable\", (char*)NULL)", file, file);
     return errno;
   }
 
